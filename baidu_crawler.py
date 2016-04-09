c39b1e9d05e6b7134667acc20427b94e74faedc7
fancoo <2680461921@qq.com>
2016-04-09 17:26:36 +0800

    a crawler

---
+++ baidu_crawler.py
@@ -0,0 +1,131 @@
+# -*- coding:utf-8 -*-
+import sys
+import requests
+from lxml import etree
+import re
+from bs4 import BeautifulSoup
+from time import sleep
+import random
+import ip_pool
+import chardet
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+"""
+================================================
+ Extract text from the result of BaiDu search
+================================================
+"""
+
+
+def download_html(keywords, proxy):
+    """
+    download html
+
+    Parameters
+    ---------
+    keywords: keywords need to be search.
+    proxy: an ip with port.
+
+    Returns
+    ------
+    utf8_content: the web content encode in utf-8.
+
+    """
+    key = {'wd': keywords}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
+    web_content = requests.get("http://www.baidu.com/s?", params=key, headers=headers, proxies=proxy, timeout=4)
+    content = web_content.text
+    return content
+
+
+def html_parser(html):
+    """
+    web parser
+
+    Parameters
+    ----------
+    html: the web html.
+
+    Returns
+    -------
+    text: the whole text of the search results.
+    times: the times that key word hits.
+
+    """
+    path = "//div[@id='content_left']//div[@class='c-abstract']/text()"  # Xpath of abstract in BaiDu search results
+    tree = etree.HTML(html)
+    results = tree.xpath(path)
+    text = [line.strip() for line in results]
+    text_str = ''
+    if len(text) == 0:
+        print "No!"
+    else:
+        for i in text:
+            i = i.strip()
+            text_str += i
+    return text_str
+
+
+def extract_all_text(keyword_dict, keyword_text):
+    """
+    ========================================================
+    Extract all text of elements in company dict
+    There are 3 strategies:
+        1. Every time appears "download timeout", I will choose another proxy.
+        2. Every 200 times after we crawl, change an proxy.
+        3. Every 2000,0 times after we crawl, Re-construct an ip_pool.
+
+    ========================================================
+    Parameters
+    ---------
+    keyword_dict: the keyword name dict.
+    keyword_text: file that save all text.
+
+    Return
+    ------
+    """
+
+    cn = open(keyword_dict, 'r')
+    print ">>>>>read success<<<"
+    with open(keyword_text, 'w') as ct:
+        flag = 0  # Change ip
+        switch = 0  # Change the proxies list
+        useful_proxies = []
+        new_ip = ''
+        for line in cn:
+            if switch % 20000 == 0:
+                switch = 1
+                ip_list = ip_pool.get_all_ip(1)
+                useful_proxies = ip_pool.get_the_best(1, ip_list, 1.5, 20)
+            switch += 1
+            try:
+                if flag % 200 == 0 and len(useful_proxies) != 0:
+                    flag = 1
+                    rd = random.randint(0, len(useful_proxies)-1)
+                    new_ip = useful_proxies[rd]
+                    print new_ip
+                flag += 1
+                proxy = {'http': 'http://'+new_ip}
+                content = download_html(line, proxy)
+                raw_text = html_parser(content)
+                raw_text = raw_text.replace('\n', ' ')
+                print raw_text
+                ct.write(line.strip()+'\t'+raw_text+'\n')
+            except Exception, e:
+                rd = random.randint(0, len(useful_proxies)-1)
+                new_ip = useful_proxies[rd]
+                print 'download error: ', e
+    ct.close()
+    cn.close()
+
+
+def main():
+    keyword_dict = 'data/samples.txt'
+    keyword_text = 'data/results.txt'
+    extract_all_text(keyword_dict, keyword_text)
+
+if __name__ == '__main__':
+    main()
+
---
+++ ip_pool.py
@@ -0,0 +1,113 @@
+# -*- coding:utf-8 -*-
+import sys
+import requests
+import time
+from lxml import etree
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+# Get all ip in a specified page
+def get_proxies_from_site(current_page):
+    """
+    Parameter
+    --------
+    current_page: int, the current page's number.
+    Return
+    ------
+    ip_list: list, a list with all 20*page ip with their corresponding ports.
+    """
+    url = 'http://www.66ip.cn/areaindex_1/'+str(current_page)+'.html'  # The ip resources url
+    url_xpath = '/html/body/div[last()]//table//tr[position()>1]/td[1]/text()'
+    port_xpath = '/html/body/div[last()]//table//tr[position()>1]/td[2]/text()'
+    results = requests.get(url)
+    tree = etree.HTML(results.text)
+    url_results = tree.xpath(url_xpath)    # Get ip
+    port_results = tree.xpath(port_xpath)  # Get port
+    urls = [line.strip() for line in url_results]
+    ports = [line.strip() for line in port_results]
+
+    ip_list = []
+    if len(urls) != len(ports):
+        print "No! It's crazy!"
+    else:
+        for i in range(len(urls)):
+            # Match each ip with it's port
+            full_ip = urls[i]+":"+ports[i]
+            print full_ip
+            ip_list.append(full_ip)
+    return ip_list
+
+
+#  Get all ip in 0~page pages website
+def get_all_ip(page):
+    ip_list = []
+    for i in range(page):
+        cur_ip_list = get_proxies_from_site(i+1)
+        for item in cur_ip_list:
+            ip_list.append(item)
+    for item in ip_list:
+        print item
+    return ip_list
+
+
+# Use http://lwons.com/wx to test if the server is available.
+def get_valid_proxies(proxies, timeout):
+    url = 'http://lwons.com/wx'
+    results = []
+    for p in proxies:
+        proxy = {'http': 'http://'+p}
+        succeed = False
+        try:
+            start = time.time()
+            r = requests.get(url, proxies=proxy, timeout=timeout)
+            end = time.time()
+            if r.text == 'default':
+                succeed = True
+        except Exception, e:
+            print 'error:', p
+            succeed = False
+        if succeed:
+            print 'succeed: '+p+'\t'+str(end-start)
+            results.append(p)
+        time.sleep(1)  # Avoid frequent crawling
+    results = list(set(results))
+    return results
+
+
+def get_the_best(round, proxies, timeout, sleeptime):
+    """
+    ========================================================
+    With the strategy of N round test to find those secure
+    and stable ip. During each round it will sleep a while to
+    avoid a 'famous 15 minutes"
+    ========================================================
+    Parameters
+    ----------
+    round: int, a number to decide how many round the test will hold.
+    proxies: list, the ip list to be detected.
+    timeout:  float, for each ip, decide the longest time we assume it's disconnected.
+    sleeptime: float, how many seconds it sleep between two round test.
+    """
+    for i in range(round):
+        print '\n'
+        print ">>>>>>>Round\t"+str(i+1)+"<<<<<<<<<<"
+        proxies = get_valid_proxies(proxies, timeout)
+        time.sleep(sleeptime)
+    return proxies
+
+
+def main():
+    ip_list = get_all_ip(2)
+    proxies = get_the_best(3, ip_list, 1.5, 60)  # The suggested parameters
+    print "\n\n\n"
+    print ">>>>>>>>>>>>>>>>>>>The Final Ip<<<<<<<<<<<<<<<<<<<<<<"
+    for item in proxies:
+        print item
+
+
+if __name__ == '__main__':
+    main()
+
+
+
